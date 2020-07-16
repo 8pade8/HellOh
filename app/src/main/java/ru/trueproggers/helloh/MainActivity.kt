d7c4657c9f46742f208b4fdf0b14048568d37ccd
8pade8 <8pade8@gmail.com>
2020-07-16 20:40:50 +0300

    #5 some refactoring

--- app/src/main/java/ru/trueproggers/helloh/MainActivity.kt
+++ app/src/main/java/ru/trueproggers/helloh/MainActivity.kt
@@ -1,27 +1,16 @@
 package ru.trueproggers.helloh
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import com.mikepenz.materialdrawer.AccountHeader
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.DividerDrawerItem
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import ru.trueproggers.helloh.databinding.ActivityMainBinding
-import ru.trueproggers.helloh.ui.ChatsFragment
-import ru.trueproggers.helloh.ui.SettingsFragment
+import ru.trueproggers.helloh.ui.fragments.ChatsFragment
+import ru.trueproggers.helloh.ui.objects.AppDrawer
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mDrawer: Drawer
-    private lateinit var mHeader: AccountHeader
+    private lateinit var mAppDrawer: AppDrawer
     private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,105 +27,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFunc() {
         setSupportActionBar(mToolbar)
+        mAppDrawer.create()
         supportFragmentManager.beginTransaction()
-            .replace(R.id.dataContainer, ChatsFragment())
+            .replace(R.id.dataContainer,
+                ChatsFragment()
+            )
             .commit()
-        createHeader()
-        createDrawer()
-    }
-
-    private fun createDrawer() {
-        mDrawer = DrawerBuilder()
-            .withActivity(this)
-            .withToolbar(mToolbar)
-            .withActionBarDrawerToggle(true)
-            .withSelectedItem(-1)
-            .withAccountHeader(mHeader)
-            .addDrawerItems(
-                PrimaryDrawerItem()
-                    .withIdentifier(100)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать группу")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_create_groups)),
-                PrimaryDrawerItem()
-                    .withIdentifier(101)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать секретный чат")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_secret_chat)),
-                PrimaryDrawerItem()
-                    .withIdentifier(102)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать канал")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_create_channel)),
-                PrimaryDrawerItem()
-                    .withIdentifier(103)
-                    .withIconTintingEnabled(true)
-                    .withName("Контакты")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_contacts)),
-                PrimaryDrawerItem()
-                    .withIdentifier(104)
-                    .withIconTintingEnabled(true)
-                    .withName("Звонки")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_phone)),
-                PrimaryDrawerItem()
-                    .withIdentifier(105)
-                    .withIconTintingEnabled(true)
-                    .withName("Избранное")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_favorites)),
-                PrimaryDrawerItem()
-                    .withIdentifier(106)
-                    .withIconTintingEnabled(true)
-                    .withName("Настройки")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_settings)),
-                DividerDrawerItem(),
-                PrimaryDrawerItem()
-                    .withIdentifier(107)
-                    .withIconTintingEnabled(true)
-                    .withName("Пригласить друзей")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_invate)),
-                PrimaryDrawerItem()
-                    .withIdentifier(108)
-                    .withIconTintingEnabled(true)
-                    .withName("Вопросы о Hell Oh")
-                    .withSelectable(false)
-                    .withIcon(getDrawable(R.drawable.ic_menu_help))
-            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
-                @SuppressLint("ShowToast")
-                override fun onItemClick(
-                    view: View?,
-                    position: Int,
-                    drawerItem: IDrawerItem<*>
-                ): Boolean {
-                    when (position) {
-                        7 -> supportFragmentManager.beginTransaction()
-                            .addToBackStack(null)
-                            .replace(R.id.dataContainer, SettingsFragment())
-                            .commit()
-                    }
-                    return false
-                }
-            }).build()
-    }
-
-    private fun createHeader() {
-        mHeader = AccountHeaderBuilder()
-            .withActivity(this)
-            .withHeaderBackground(R.drawable.header)
-            .addProfiles(
-                ProfileDrawerItem().withName("Mikhail Tatishchev")
-                    .withEmail("+71111111111")
-            ).build()
     }
 
     private fun initFields() {
         mToolbar = mBinding.mainToolbar
+        mAppDrawer = AppDrawer(this, mToolbar)
     }
 }
