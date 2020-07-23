c3c7d0ef46395283f3f6ce0b4ac892bf086b8fe9
8pade8 <8pade8@gmail.com>
2020-07-23 22:22:11 +0300

    #5 some refactoring

---
+++ app/src/main/java/ru/trueproggers/helloh/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,10 @@
+package ru.trueproggers.helloh.ui.fragments
+
+import ru.trueproggers.helloh.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/ru/trueproggers/helloh/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package ru.trueproggers.helloh.ui.fragments
+
+import ru.trueproggers.helloh.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
