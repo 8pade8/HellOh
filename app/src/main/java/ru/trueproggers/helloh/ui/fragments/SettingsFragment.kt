73aa1682160fe59602259ae9f4c35fdb8e0c46de
Дмитрий <dimassokol@mail.ru>
2020-07-23 23:01:57 +0300

    #7 add action menu for SettingsFragment

--- app/src/main/java/ru/trueproggers/helloh/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ru/trueproggers/helloh/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,18 @@
 package ru.trueproggers.helloh.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import ru.trueproggers.helloh.R
+import ru.trueproggers.helloh.R.menu.settings_action_menu
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(settings_action_menu, menu)
     }
 }
