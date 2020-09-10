25469fc4436d6cf7b3f38add8c982f1fc868f29e
boJlek <dimassokol@mail.ru>
2020-09-10 19:02:05 +0300

    #9 refactoring

--- app/src/main/java/ru/trueproggers/helloh/ui/fragments/BaseFragment.kt
+++ app/src/main/java/ru/trueproggers/helloh/ui/fragments/BaseFragment.kt
@@ -1,22 +1,8 @@
 package ru.trueproggers.helloh.ui.fragments
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-open class BaseFragment(val layout: Int) : Fragment() {
-
-    private lateinit var mRootView: View
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mRootView = inflater.inflate(layout, container, false)
-        return mRootView
-    }
+open class BaseFragment(layout: Int) : Fragment(layout) {
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/ru/trueproggers/helloh/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/ru/trueproggers/helloh/ui/fragments/EnterCodeFragment.kt
@@ -6,28 +6,21 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_enter_code.*
 import ru.trueproggers.helloh.R
+import ru.trueproggers.helloh.utilits.AppTextWatcher
+import ru.trueproggers.helloh.utilits.showToast
 
 class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
     override fun onStart() {
         super.onStart()
-        register_input_code.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val string = register_input_code.text.toString()
-                if (string.length == 6) {
-                    verifiCode()
-                }
+        register_input_code.addTextChangedListener(AppTextWatcher {
+            val string = register_input_code.text.toString()
+            if (string.length == 6) {
+                verifiCode()
             }
-
-            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
         })
     }
 
     fun verifiCode() {
-        Toast.makeText(activity, "Ok", Toast.LENGTH_SHORT).show()
+        showToast("OK")
     }
 }
